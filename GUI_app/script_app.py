e756db94d511a761a6d5adeaebdc70a002d18e14
iamgeorgp <plus.george@yandex.ru>
2023-11-22 22:25:50 +0300

    add new buttons and inserting

--- GUI_app/script_app.py
+++ GUI_app/script_app.py
@@ -1,9 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy, QSizePolicy, QDesktopWidget, QMessageBox, QComboBox, QTextEdit, QTableWidget, QHBoxLayout, QSplitter, QScrollArea, QFileDialog, QTableWidgetItem, QApplication
+from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy, QSizePolicy, QDesktopWidget, QMessageBox, QComboBox, QTextEdit, QTableWidget, QHBoxLayout, QSplitter, QScrollArea, QFileDialog, QTableWidgetItem, QApplication, QDateEdit
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt
 import csv
 import sqlite3
+from PIL import Image, ImageDraw, ImageFont
 import os
 
 class LoginApp(QWidget):
@@ -93,8 +94,8 @@ class LoginApp(QWidget):
         self.forget_password_button = QPushButton('Forgot password?', objectName='forgetPasswordButton')
 
         # # Default inputs
-        # self.login_input.setText('admin')
-        # self.password_input.setText('admin')
+        self.login_input.setText('admin')
+        self.password_input.setText('admin')
 
         # Connecting buttons with functions
         self.login_button.clicked.connect(self.check_credentials)
@@ -267,11 +268,11 @@ class LoginApp(QWidget):
         result = cursor.fetchall()
         cities = [row[0] for row in result]
 
-        cursor.execute("SELECT DISTINCT service FROM service")
+        cursor.execute("SELECT DISTINCT servicename FROM service")
         result_services = cursor.fetchall()
         unique_services = [row[0] for row in result_services]
 
-        cursor.execute("SELECT DISTINCT TypeService FROM typeservice")
+        cursor.execute("SELECT DISTINCT TypeServicename FROM typeservice")
         result = cursor.fetchall()
         unique_type_services = [row[0] for row in result]
 
@@ -360,6 +361,17 @@ class LoginApp(QWidget):
         upper_left_layout = QVBoxLayout()
         down_left_layout = QVBoxLayout()
 
+        upper_left_button_layout = QHBoxLayout()
+        change_button = QPushButton('Change Data') 
+        add_button = QPushButton('Add Data')
+        add_button.clicked.connect(self.add_data_screen)
+        delete_button = QPushButton('Delete Data')
+
+        upper_left_button_layout.addWidget(change_button)
+        upper_left_button_layout.addWidget(add_button)
+        upper_left_button_layout.addWidget(delete_button)
+
+        upper_left_layout.addLayout(upper_left_button_layout)
         # The upper layout consists of a header, two drop-down lists, a script input block and a start button
         upper_left_layout.addWidget(self.sql_label)
         upper_left_layout.addWidget(self.action_combo_box)
@@ -427,6 +439,264 @@ class LoginApp(QWidget):
         self.sql_window.showFullScreen()
 
 
+    def add_data_screen(self):
+        self.add_data_window = QWidget()
+        
+        
+        
+        # main add data window layout
+        main_layout = QVBoxLayout()
+
+        # init  layout of adding data
+        add_data_in_table = QHBoxLayout()
+
+        # init of box clumns
+        company_layout = QVBoxLayout()
+        client_layout = QVBoxLayout()
+        type_service_layout = QVBoxLayout()
+        service_layout = QVBoxLayout()
+        manager_layout = QVBoxLayout()
+
+        # Add column "Company Table"
+        company_label = QLabel('Company Table')
+        company_input_label = QLabel('Enter name')
+        self.company_input = QLineEdit()
+        company_add_button = QPushButton('Add')
+        company_add_button.clicked.connect(self.add_company)
+
+        company_layout.addWidget(company_label)
+        company_layout.addWidget(company_input_label)
+        company_layout.addWidget(self.company_input)
+        company_layout.addWidget(company_add_button)
+
+        # add column "Client Table"
+        client_label = QLabel('Сlient Table')
+        client_input_layout = QHBoxLayout()
+
+        client_company_choose_layout = QVBoxLayout()
+        client_company_choose_label = QLabel("Choose Company")
+        self.client_company_choose = QComboBox()
+        try:
+            conn = sqlite3.connect('app_files\\EGA_database.db')
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT DISTINCT companyname FROM Company")
+            companies = cursor.fetchall()
+
+            for company in companies:
+                self.client_company_choose.addItem(company[0])
+
+            conn.close()
+        except sqlite3.Error as e:
+            print(f"Error: {str(e)}")
+        
+        client_company_choose_layout.addWidget(client_company_choose_label)
+        client_company_choose_layout.addWidget(self.client_company_choose)
+
+        client_name_input_layout = QVBoxLayout()
+        client_name_input_label = QLabel("Enter name")
+        self.client_name_input = QLineEdit()
+        client_name_input_layout.addWidget(client_name_input_label)
+        client_name_input_layout.addWidget(self.client_name_input)
+
+        client_city_input_layout = QVBoxLayout()
+        client_city_input_label = QLabel("Enter city")
+        self.client_city_input = QLineEdit()
+        client_city_input_layout.addWidget(client_city_input_label)
+        client_city_input_layout.addWidget(self.client_city_input)
+        
+        client_address_input_layout = QVBoxLayout()
+        client_address_input_label = QLabel("Enter address")
+        self.client_address_input = QLineEdit()
+        client_address_input_layout.addWidget(client_address_input_label)
+        client_address_input_layout.addWidget(self.client_address_input)
+        
+        client_phone_input_layout = QVBoxLayout()
+        client_phone_input_label = QLabel("Enter phone")
+        self.client_phone_input = QLineEdit()
+        client_phone_input_layout.addWidget(client_phone_input_label)
+        client_phone_input_layout.addWidget(self.client_phone_input)
+
+        
+
+        client_input_layout.addLayout(client_company_choose_layout)
+        client_input_layout.addLayout(client_name_input_layout)
+        client_input_layout.addLayout(client_city_input_layout)
+        client_input_layout.addLayout(client_address_input_layout)
+        client_input_layout.addLayout(client_phone_input_layout)
+
+        client_add_button = QPushButton('Add')
+        client_add_button.clicked.connect(self.add_client)
+
+        client_layout.addWidget(client_label)
+        client_layout.addLayout(client_input_layout)
+        client_layout.addWidget(client_add_button)
+
+        # Add column "TypeService Table"
+        type_service_label = QLabel('TypeService Table')
+        type_service_input_label = QLabel('Enter name')
+        self.type_service_input = QLineEdit()
+        type_service_add_button = QPushButton('Add')
+        type_service_add_button.clicked.connect(self.add_type_service)
+
+        type_service_layout.addWidget(type_service_label)
+        type_service_layout.addWidget(type_service_input_label)
+        type_service_layout.addWidget(self.type_service_input)
+        type_service_layout.addWidget(type_service_add_button)
+
+        # Add column "Service Table"
+        service_label = QLabel('Service Table')
+
+        service_input_layout = QHBoxLayout()
+
+        choose_type_service_layout = QVBoxLayout()
+        choose_type_service_label = QLabel('Choose Type Service')
+        choose_type_service = QComboBox()
+        choose_type_service_layout.addWidget(choose_type_service_label)
+        choose_type_service_layout.addWidget(choose_type_service)
+
+        service_name_layout = QVBoxLayout()
+        service_name_label = QLabel('Enter name')
+        service_name_input = QLineEdit()
+        service_name_layout.addWidget(service_name_label)
+        service_name_layout.addWidget(service_name_input)
+        
+        service_add_button = QPushButton('Add')
+
+        service_input_layout.addLayout(choose_type_service_layout)
+        service_input_layout.addLayout(service_name_layout)
+
+        service_layout.addWidget(service_label)
+        service_layout.addLayout(service_input_layout)
+        service_layout.addWidget(service_add_button)
+
+        
+
+        manager_label = QLabel("Manager Table")
+        manager_input_layout = QHBoxLayout()
+
+        manager_name_layout = QVBoxLayout()
+        manager_name_label = QLabel("Enter Name")
+        manager_name_input = QLineEdit()
+        manager_name_layout.addWidget(manager_name_label)
+        manager_name_layout.addWidget(manager_name_input)
+
+        manager_phone_layout = QVBoxLayout()
+        manager_phone_label = QLabel("Enter Phone")
+        manager_phone_input = QLineEdit()
+        manager_phone_layout.addWidget(manager_phone_label)
+        manager_phone_layout.addWidget(manager_phone_input)
+
+        manager_input_layout.addLayout(manager_name_layout)
+        manager_input_layout.addLayout(manager_phone_layout)
+
+        manager_add_button = QPushButton('Add')
+
+        manager_layout.addWidget(manager_label)
+        manager_layout.addLayout(manager_input_layout)
+        manager_layout.addWidget(manager_add_button)
+
+        add_data_in_table.addLayout(company_layout)
+        add_data_in_table.addLayout(client_layout)
+        add_data_in_table.addLayout(type_service_layout)
+        add_data_in_table.addLayout(service_layout)
+        add_data_in_table.addLayout(manager_layout)
+
+        main_layout.addLayout(add_data_in_table)
+
+
+        self.add_data_window.setLayout(main_layout)
+        self.add_data_window.show()
+
+    def add_company(self) -> None:
+        company_name = self.company_input.text()
+        print(company_name)
+        if company_name != '':
+            try:
+                conn = sqlite3.connect('app_files\\EGA_database.db')
+                cursor = conn.cursor()
+
+                cursor.execute("SELECT * FROM Company WHERE companyname = ?", (company_name,))
+                existing_company = cursor.fetchone()
+
+                if existing_company:
+                    QMessageBox.warning(self.sql_window, 'Existing company', 'Company with this name already exists.')
+                else:
+                    cursor.execute("INSERT INTO Company (companyid, companyname) SELECT COALESCE(MAX(companyid), 0) + 1, ? FROM Company", (company_name,))
+                    conn.commit()
+                    self.client_company_choose.addItem(company_name)
+                    QMessageBox.information(self.sql_window, 'Success', 'Company added successfully.')
+
+                conn.close()
+            except sqlite3.Error as e:
+                QMessageBox.warning(self.sql_window, 'Query execution error', f'Error: {str(e)}')
+    
+        self.company_input.clear()
+        self.add_data_window.raise_()
+
+    def add_client(self) -> None:
+        client_name = self.client_name_input.text()
+        client_city = self.client_city_input.text()
+        client_address = self.client_address_input.text()
+        client_phone = self.client_phone_input.text()
+        client_company_name = self.client_company_choose.currentText()
+
+        if client_name != '' and client_city != '' and client_address != '' and client_phone != '':
+            try:
+                conn = sqlite3.connect('app_files\\EGA_database.db')
+                cursor = conn.cursor()
+
+                cursor.execute("SELECT * FROM Clients WHERE clientname = ?", (client_name,))
+                existing_company = cursor.fetchone()
+                if existing_company:
+                    QMessageBox.warning(self.sql_window, 'Existing client', 'Client with this name already exists.')
+                else:
+                    cursor.execute("SELECT MAX(clientid) FROM clients")
+                    max_client_id = cursor.fetchone()[0]
+                    print(max_client_id)
+                    new_client_id = max_client_id + 1 if max_client_id else 1
+
+                    cursor.execute("SELECT companyid FROM company WHERE companyname = ?", (client_company_name,))
+                    company_id = cursor.fetchone()[0]
+                    print(company_id)
+
+                    cursor.execute("INSERT INTO clients (clientid, companyid, clientname, address, city, clientphone) VALUES (?, ?, ?, ?, ?, ?)",
+                           (new_client_id, company_id, client_name, client_address, client_city, client_phone))
+                    conn.commit()
+                    QMessageBox.information(self.sql_window, 'Success', 'Company added successfully.')
+            except sqlite3.Error as e:
+                QMessageBox.warning(self, 'Query execution error', f'Error: {str(e)}')
+        conn.close()         
+        self.client_name_input.clear()
+        self.client_city_input.clear()
+        self.client_address_input.clear()
+        self.client_phone_input.clear()
+        self.add_data_window.raise_()
+
+    def add_type_service(self) -> None:
+        type_service = self.type_service_input.text()
+        print(type_service)
+        if type_service != '':
+            try:
+                conn = sqlite3.connect('app_files\\EGA_database.db')
+                cursor = conn.cursor()
+
+                cursor.execute("SELECT * FROM typeservice WHERE typeservicename = ?", (type_service,))
+                existing_company = cursor.fetchone()
+
+                if existing_company:
+                    QMessageBox.warning(self.sql_window, 'Existing company', 'Company with this name already exists.')
+                else:
+                    cursor.execute("INSERT INTO typeservice (typeserviceid, typeservicename) SELECT COALESCE(MAX(typeserviceid), 0) + 1, ? FROM typeservice", (type_service,))
+                    conn.commit()
+                    QMessageBox.information(self.sql_window, 'Success', 'Company added successfully.')
+
+                conn.close()
+            except sqlite3.Error as e:
+                QMessageBox.warning(self.sql_window, 'Query execution error', f'Error: {str(e)}')
+    
+        self.type_service_input.clear()
+        self.add_data_window.raise_()
 
     def load_contract_image(self) -> None:
         """
@@ -435,25 +705,83 @@ class LoginApp(QWidget):
         Retrieves the contract number input and saves the corresponding image file.
         Displays success or error messages based on the process outcome.
         """
-
         contract_number = self.contract_number_input.text() # Select number of contract
-        if contract_number.isdigit():   # Check for digit
-            file_path, _ = QFileDialog.getSaveFileName(self, 'Save file', f"contract_{contract_number}.jpg", "Images (*.jpg *.png)")
-            if file_path:
-                # Scaning dir for contratcs
-                source_file_path = f"app_files\\scan_contract\\contract_{contract_number}.jpg"
-                if os.path.exists(source_file_path):
-                    try:
-                        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                        with open(source_file_path, 'rb') as src_file, open(file_path, 'wb') as dest_file:
-                            dest_file.write(src_file.read())
-                        QMessageBox.information(self, 'success', f"Contract image {contract_number} has been successfully saved")
-                    except Exception as e:
-                        QMessageBox.warning(self, 'Error', f"Failed to save file: {e}")
-                else:
-                    QMessageBox.warning(self, 'Error', f"Contract image {contract_number} not found")
-        else:
-            QMessageBox.warning(self, 'Error', 'Enter contract number (number)')
+        # Loading a contract template
+        contract_template = Image.open(f'app_files\\default.jpg')  
+
+        # Выбор шрифта и его размера
+        font = ImageFont.truetype('arial.ttf', size=10) 
+
+        # Create an ImageDraw object for image editing
+        draw = ImageDraw.Draw(contract_template)
+
+        # Set the position and text for each field
+        text_positions = {
+            'Signing date': (400, 100),
+            'Contract ID': (270, 130),
+            'Service': (20, 300),
+            'Company name': (20, 320),
+            'Client name': (320, 400),
+            'Phone number': (320, 420),
+            'Start date': (320, 440),
+            'End date': (320, 460),
+            'Pay date': (320, 480),
+            'Price': (320, 500)
+        }
+
+        try:
+            conn = sqlite3.connect('app_files\\EGA_database.db')
+            cursor = conn.cursor()
+            cursor.execute(f"select signingdate, contractid, service,  companyname, clientname, clientphone, startdate, enddate, paydate, price from contracts join TypeService using(TypeServiceID) join service using(serviceid) join company using(companyid) join clients using(clientid) join managers using(managerid) where contractid = '{contract_number}'")
+            rows = cursor.fetchall()
+            
+
+            conn.close()
+        except sqlite3.Error as e:
+            QMessageBox.warning(self.sql_window, 'Query execution error', f'Error: {str(e)}')
+
+        contract_fields = {
+            'Signing date': rows[0][0],
+            'Contract ID': rows[0][1],
+            'Service': rows[0][2],
+            'Company name': rows[0][3],
+            'Client name': rows[0][4],
+            'Phone number': rows[0][5],
+            'Start date': rows[0][6],
+            'End date': rows[0][7],
+            'Pay date': rows[0][8],
+            'Price': str(rows[0][9]) + '$'
+        }
+
+        # Add the text of specified fields to the image
+        for field, position in text_positions.items():
+            if field in contract_fields:
+                draw.text(position, f"{field}: {contract_fields[field]}", font=font, fill='black')
+
+        # Saving the modified image to a file with a unique name for each line
+        # Compression with quality parameter added
+        # contract_template.save(f'{gen_data_directory}\contract_{fields["Contract ID"]}.jpg', quality=80)  
+        file_path, _ = QFileDialog.getSaveFileName(self, 'Save file', f"contract_{contract_number}.jpg", "Images (*.jpg *.png)")
+        contract_template.save(f'{file_path}', quality=80)  
+
+        # contract_number = self.contract_number_input.text() # Select number of contract
+        # if contract_number.isdigit():   # Check for digit
+        #     file_path, _ = QFileDialog.getSaveFileName(self, 'Save file', f"contract_{contract_number}.jpg", "Images (*.jpg *.png)")
+        #     if file_path:
+        #         # Scaning dir for contratcs
+        #         source_file_path = f"app_files\\scan_contract\\contract_{contract_number}.jpg"
+        #         if os.path.exists(source_file_path):
+        #             try:
+        #                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        #                 with open(source_file_path, 'rb') as src_file, open(file_path, 'wb') as dest_file:
+        #                     dest_file.write(src_file.read())
+        #                 QMessageBox.information(self, 'success', f"Contract image {contract_number} has been successfully saved")
+        #             except Exception as e:
+        #                 QMessageBox.warning(self, 'Error', f"Failed to save file: {e}")
+        #         else:
+        #             QMessageBox.warning(self, 'Error', f"Contract image {contract_number} not found")
+        # else:
+        #     QMessageBox.warning(self, 'Error', 'Enter contract number (number)')
 
 
 
